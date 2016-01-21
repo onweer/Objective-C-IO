655b8e7e560b58eae19381598cf5e62b99cca1b5
Zongminglee <zongmingli@outlook.com>
2016-01-21 14:25:07 +0800

    创建OC文件IO的学习项目

---
+++ Objective-C-IO/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C-IO
+//
+//  Created by SpiColorPendra on 16/1/21.
+//  Copyright © 2016年 SpiColorPendra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
