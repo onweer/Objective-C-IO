d6d24d63f91003f67d1e7d8047abd80b0b77bc11
Zongminglee <zongmingli@outlook.com>
2016-01-22 11:13:54 +0800

    add 对象归档

---
+++ 对象归档/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  对象归档
+//
+//  Created by SpiColorPendra on 16/1/22.
+//  Copyright © 2016年 SpiColorPendra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
